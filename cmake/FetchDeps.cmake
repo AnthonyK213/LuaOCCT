7ef284c1390ecf4b5ac8df7054067bffe19c758f
AnthonyK213 <tjjzqwh@163.com>
2024-03-07 20:10:45 +0800

    add luajit

--- cmake/FetchDeps.cmake
+++ cmake/FetchDeps.cmake
@@ -14,6 +14,22 @@ FetchContent_Declare(
   SOURCE_DIR ${CMAKE_BINARY_DIR}/deps/LuaBridge3
   )
 
+FetchContent_Declare(
+  LuaJIT_Source
+  GIT_REPOSITORY https://github.com/LuaJIT/LuaJIT
+  GIT_TAG origin/v2.1
+  SOURCE_DIR ${CMAKE_BINARY_DIR}/deps/LuaJIT_Source
+  CONFIGURE_COMMAND ""
+  BUILD_COMMAND ""
+  )
+
+FetchContent_Declare(
+  LuaJIT
+  GIT_REPOSITORY https://github.com/zhaozg/luajit-cmake
+  GIT_TAG origin/master
+  SOURCE_DIR ${CMAKE_BINARY_DIR}/deps/luajit-cmake
+  )
+
 # FetchContent_MakeAvailable(binder)
 FetchContent_GetProperties(binder)
 if(NOT binder_POPULATED)
@@ -33,5 +49,28 @@ if(NOT luabridge3_POPULATED)
     ${luabridge3_SOURCE_DIR}
     ${luabridge3_BINARY_DIR}
     EXCLUDE_FROM_ALL
+    )
+endif()
+
+# FetchContent_MakeAvailable(LuaBridge3)
+FetchContent_GetProperties(LuaJIT_Source)
+if(NOT luajit_source_POPULATED)
+  FetchContent_Populate(LuaJIT_Source)
+endif()
+
+# FetchContent_MakeAvailable(LuaBridge3)
+FetchContent_GetProperties(LuaJIT)
+if(NOT luajit_POPULATED)
+  FetchContent_Populate(LuaJIT)
+  add_subdirectory(
+    ${luajit_SOURCE_DIR}
+    ${luajit_BINARY_DIR}
+    EXCLUDE_FROM_ALL
+    )
+
+  target_include_directories(
+    buildvm
+    PRIVATE
+    ${luajit_BINARY_DIR}
     )
 endif()
