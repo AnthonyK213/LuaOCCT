3d6eb2e1cb34e5316684f323a52dd8e200c25c7b
AnthonyK213 <tjjzqwh@163.com>
2024-03-07 14:08:52 +0800

    deps: luabridge3

--- cmake/FetchDeps.cmake
+++ cmake/FetchDeps.cmake
@@ -7,6 +7,13 @@ FetchContent_Declare(
   SOURCE_DIR ${CMAKE_BINARY_DIR}/deps/binder
   )
 
+FetchContent_Declare(
+  LuaBridge3
+  GIT_REPOSITORY https://github.com/kunitoki/LuaBridge3.git
+  GIT_TAG origin/master
+  SOURCE_DIR ${CMAKE_BINARY_DIR}/deps/LuaBridge3
+  )
+
 # FetchContent_MakeAvailable(binder)
 FetchContent_GetProperties(binder)
 if(NOT binder_POPULATED)
@@ -14,6 +21,17 @@ if(NOT binder_POPULATED)
   add_subdirectory(
     ${binder_SOURCE_DIR}
     ${binder_BINARY_DIR}
+    EXCLUDE_FROM_ALL
+    )
+endif()
+
+# FetchContent_MakeAvailable(LuaBridge3)
+FetchContent_GetProperties(LuaBridge3)
+if(NOT luabridge3_POPULATED)
+  FetchContent_Populate(LuaBridge3)
+  add_subdirectory(
+    ${luabridge3_SOURCE_DIR}
+    ${luabridge3_BINARY_DIR}
     EXCLUDE_FROM_ALL
     )
 endif()
