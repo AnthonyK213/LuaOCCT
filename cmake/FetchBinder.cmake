f302c468fb0e15b747f3bae2edc7efe4cd1d2c92
AnthonyK213 <tjjzqwh@163.com>
2024-03-16 14:16:57 +0800

    set env to use local repo

--- cmake/FetchBinder.cmake
+++ cmake/FetchBinder.cmake
@@ -1,11 +1,19 @@
 include(FetchContent)
 
-FetchContent_Declare(
-  binder
-  GIT_REPOSITORY https://github.com/AnthonyK213/LuaOCCT-binder
-  GIT_TAG origin/master
-  SOURCE_DIR ${CMAKE_BINARY_DIR}/deps/binder
-  )
+if (DEFINED ENV{LUAOCCT_BINDER_DIR})
+  FetchContent_Declare(
+    binder
+    SOURCE_DIR $ENV{LUAOCCT_BINDER_DIR}
+    )
+  message("Use local LuaOCCT-binder: $ENV{LUAOCCT_BINDER_DIR}")
+else()
+  FetchContent_Declare(
+    binder
+    GIT_REPOSITORY https://github.com/AnthonyK213/LuaOCCT-binder
+    GIT_TAG origin/master
+    SOURCE_DIR ${CMAKE_BINARY_DIR}/deps/binder
+    )
+endif()
 
 # FetchContent_MakeAvailable(binder)
 FetchContent_GetProperties(binder)
