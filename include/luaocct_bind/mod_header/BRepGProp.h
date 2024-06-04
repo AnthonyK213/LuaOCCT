d0303cf469dea19a339cadbadc9ce00f2c8c7d75
Anthony <tjjzqwh@163.com>
2024-06-04 12:03:30 +0800

    bind: brepgprop_xxx

--- include/luaocct_bind/mod_header/BRepGProp.h
+++ include/luaocct_bind/mod_header/BRepGProp.h
@@ -2,5 +2,17 @@
 #define _LuaOCCT_mod_header_BRepGProp_HeaderFile
 
 #include <BRepGProp.hxx>
+#include <BRepGProp_Cinert.hxx>
+#include <BRepGProp_Domain.hxx>
+#include <BRepGProp_EdgeTool.hxx>
+#include <BRepGProp_Face.hxx>
+#include <BRepGProp_MeshCinert.hxx>
+// #include <BRepGProp_MeshProps.hxx>
+// #include <BRepGProp_Gauss.hxx>
+#include <BRepGProp_Sinert.hxx>
+// #include <BRepGProp_TFunction.hxx>
+// #include <BRepGProp_UFunction.hxx>
+#include <BRepGProp_Vinert.hxx>
+#include <BRepGProp_VinertGK.hxx>
 
 #endif
