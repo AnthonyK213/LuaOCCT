b847505ba20affe9fd373f8ec7ef9ee1db01619b
AnthonyK213 <tjjzqwh@163.com>
2024-06-19 22:23:50 +0800

    update: binder

--- include/luaocct_bind/mod_header/NCollection.h
+++ include/luaocct_bind/mod_header/NCollection.h
@@ -1,12 +1,6 @@
 #ifndef _LuaOCCT_mod_header_NCollection_HeaderFile
 #define _LuaOCCT_mod_header_NCollection_HeaderFile
 
-#include <NCollection_Array1.hxx>
-#include <NCollection_Array2.hxx>
-#include <NCollection_List.hxx>
-#include <NCollection_Sequence.hxx>
-#include <NCollection_Vec2.hxx>
-#include <NCollection_Vec3.hxx>
-#include <NCollection_Vec4.hxx>
+#include <NCollection_BaseMap.hxx>
 
 #endif
--- include/luaocct_bind/mod_header/TopTools.h
+++ include/luaocct_bind/mod_header/TopTools.h
@@ -1,4 +1,8 @@
 #ifndef _LuaOCCT_mod_header_TopTools_HeaderFile
 #define _LuaOCCT_mod_header_TopTools_HeaderFile
 
+#include <TopTools_IndexedDataMapOfShapeListOfShape.hxx>
+#include <TopTools_IndexedMapOfShape.hxx>
+#include <TopTools_MapOfShape.hxx>
+
 #endif
